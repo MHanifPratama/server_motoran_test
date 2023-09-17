9c67258a2e86975b0524be2f187f17c81f48fc7e
MHanifPratama <mhanifp27@gmail.com>
2023-09-17 14:12:21 +0700

    Make Endpoint

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,5 @@ $routes->get('/booking_service', 'BookingService::index');
 $routes->get('/booking_service/booking', 'BookingService::booking');
 $routes->post('/api/v1/ddms/booking/setstatus', 'EndPoint::booking_setstatus');
 $routes->post('/api/v1/ddms/booking/information-detail', 'EndPoint::information_detail');
+$routes->post('/api/v1/ddms/booking/booking_checkout', 'EndPoint::booking_checkout');
+$routes->post('/api/v1/ddms/booking/pay', 'EndPoint::booking_pay');
--- app/Controllers/EndPoint.php
+++ app/Controllers/EndPoint.php
@@ -16,16 +16,27 @@ class EndPoint extends BaseController
     {
         return view('welcome_message');
     }
+    public function  check_credentials($data){
+        
+    }
     public function booking_setstatus()
     {
         $data = (array) $this->request->getVar();
         $apps_booking_number = $data['AppsBookingNumber'];
-        $apps_booking_number = $data['AppsBookingNumber'];
+        $dms_booking_number = $data['DmsBookingNumber'];
         $apps_booking_number_sistem = $this->akunSistem->getAkunSistem()[0]['apps_booking_number'];
         if ($apps_booking_number != $apps_booking_number_sistem){
             $response_data = [
                 'Status' => 0,
                 'Message' => 'Booking number not found',
+                'data' => '[]'
+            ];
+            return $this->respond($response_data,200);
+        }
+        if (!$dms_booking_number){
+            $response_data = [
+                'Status' => 0,
+                'Message' => 'Service Update Failed',
                 'data' => '[]'
             ];
             return $this->respond($response_data,200);
@@ -84,6 +95,33 @@ class EndPoint extends BaseController
             return $this->respond($response_data,200);
         }
         if (!$dms_booking_number){
+            $response_data = [
+                'Status' => 0,
+                'Message' => 'Service Update Failed'
+            ];
+            return $this->respond($response_data,200);
+        }
+        $response_data = [
+            'Status' => 1,
+            'Message' => 'Data successfully submitted.'
+        ];
+        return $this->respond($response_data, 200);
+    }
+    public function booking_pay(){
+        $data = (array)$this->request->getVar();
+        $apps_booking_number = $data['AppsBookingNumber'];
+        $dms_booking_number = $data['DmsBookingNumber'];
+        $amount = $data['Amount'];
+        $channel = $data['Channel'];
+        $apps_booking_number_sistem = $this->akunSistem->getAkunSistem()[0]['apps_booking_number'];
+        if ($apps_booking_number != $apps_booking_number_sistem){
+            $response_data = [
+                'Status' => 0,
+                'Message' => 'Booking number not found'
+            ];
+            return $this->respond($response_data,200);
+        }
+        if(!$dms_booking_number || !$amount || !$channel){
             $response_data = [
                 'Status' => 0,
                 'Message' => 'Service Update Failed'
